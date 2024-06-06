fix mem overflow, torchinfo

dreamer.py
@@ -350,7 +350,7 @@ def main(config):
             }
             torch.save(items_to_save, logdir / "latest.pt")
             logger.info(f"Saved model to {logdir / 'latest.pt'}")
-            # pbar.update(agent._step-pbar.n) # 16858 at a time
+            pbar.update(agent._step-pbar.n) # 16858 at a time
     for env in train_envs + eval_envs:
         try:
             env.close()
@@ -358,6 +358,7 @@ def main(config):
             pass
 
 def parse_args(argv=None):
+    # first load config name as arg from command line
     parser = argparse.ArgumentParser()
     parser.add_argument("--configs", nargs="+")
     if argv is None:
@@ -382,11 +383,16 @@ def parse_args(argv=None):
     for name in name_list:
         recursive_update(defaults, configs[name])
     
+    # defaults = {k:tools.args_type(v)(v) for k, v in defaults.items()}
+    # config = argparse.Namespace(**defaults)
+
+    # now use argparse to parse config, allowing us to override config with any extra args from cli. You can even use -h
     parser = argparse.ArgumentParser()
     for key, value in sorted(defaults.items(), key=lambda x: x[0]):
         arg_type = tools.args_type(value)
         parser.add_argument(f"--{key}", type=arg_type, default=arg_type(value))
     args = parser.parse_args(remaining)
+    logger.info(f"config={args}")
     return args
 
 if __name__ == "__main__":

models.py
@@ -5,7 +5,7 @@ from torch import nn
 import networks
 import tools
 from loguru import logger
-
+from torchinfo import summary
 from envs.craftax_env import state2img
 
 to_np = lambda x: x.detach().cpu().numpy()
@@ -99,9 +99,7 @@ class WorldModel(nn.Module):
             opt=config.opt,
             use_amp=self._use_amp,
         )
-        logger.info(
-            f"Optimizer model_opt has {sum(param.numel() for param in self.parameters())} variables."
-        )
+        logger.info(f"World Model\n{summary(self, row_settings=['var_names'],)}")
         # other losses are scaled by 1.0.
         self._scales = dict(
             reward=config.reward_head["loss_scale"],

tools.py
@@ -15,7 +15,7 @@ from torch import nn
 from torch.nn import functional as F
 from torch import distributions as torchd
 from torch.utils.tensorboard import SummaryWriter
-
+from contextlib import contextmanager
 
 to_np = lambda x: x.detach().cpu().numpy()
 
@@ -82,7 +82,7 @@ class Logger:
             scalars.append(("fps", self._compute_fps(step)))
         # print out the episode stats
         stats = " / ".join(f"{k.replace('log_achievement_', '')} <red>{v:.1f}</red>" for k, v in scalars)
-        logger.opt(colors=True).info(f"[{step}] {stats}")
+        logger.opt(colors=True).debug(f"[{step}] {stats}")
         with (self._logdir / "metrics.jsonl").open("a") as f:
             f.write(json.dumps({"step": step, **dict(scalars)}) + "\n")
         for name, value in scalars:
@@ -127,6 +127,14 @@ class Logger:
         self._writer.add_video(name, value, step, 16)
 
 
+@contextmanager
+def cond_tqdm(pbar=None, *args, **kwargs):
+    if pbar is None:
+        with tqdm(*args, **kwargs) as pbar:
+            yield pbar
+    else:
+        yield pbar
+
 def simulate(
     agent,
     envs,
@@ -151,7 +159,7 @@ def simulate(
         reward = [0] * len(envs)
     else:
         step, episode, done, length, obs, agent_state, reward = state
-    with tqdm(total=steps, disable=pbar is None) as pbar:
+    with cond_tqdm(total=steps, pbar=pbar) as pbar:
         while (steps and step < steps) or (episodes and episode < episodes):
             # reset envs if necessary
             if done.any():